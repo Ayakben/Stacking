c103a82acdb9a6d8470c10a1cdf1690196ca94f1
Ayakben <47049290+Ayakben@users.noreply.github.com>
2020-02-01 02:53:51 -0500

    Fixed Score Scipt

--- Game Jam Game/Assets/Scripts/ObjectScoreChange.cs
+++ Game Jam Game/Assets/Scripts/ObjectScoreChange.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetScore : MonoBehaviour
+public class ObjectScoreChange : MonoBehaviour
 {
-    public int score;
+    // Start is called before the first frame update
     void Start()
     {
-        score = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Score.SetScore(3);
     }
 }
---
+++ Game Jam Game/Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private static int score;
+    public static void SetScore(int value)
+    {
+        score = value;
+    }
+    public static int GetScore()
+    {
+        return score;
+    }
+}
