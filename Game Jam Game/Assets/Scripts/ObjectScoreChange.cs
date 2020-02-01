f8cc9a838d21558c30965f0860920ec564544a22
Ayakben <47049290+Ayakben@users.noreply.github.com>
2020-02-01 06:33:50 -0500

    Change the Score Change Script

--- Game Jam Game/Assets/Scripts/ObjectScoreChange.cs
+++ Game Jam Game/Assets/Scripts/ObjectScoreChange.cs
@@ -6,6 +6,7 @@ public class ObjectScoreChange : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
     public float speed;
+    public bool sleep;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +15,27 @@ public class ObjectScoreChange : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        if(rigidbody.IsSleeping())
+        {
+            sleep = true;
+        }
+        else
+        {
+            sleep = false;
+        }
         speed = rigidbody.velocity.magnitude;
         //Sets score to the y position of the object
         int pos = (int) transform.position.y;
-        if(pos > Score.GetScore())
+        if (pos > Score.GetScore())
         {
-            if(Input.GetMouseButtonUp(0))
+            if (!(Input.GetMouseButton(0)))
             {
-                Score.SetScore(pos);
+                if (rigidbody.IsSleeping())
+                {
+                    Score.SetScore(pos);
+                }
             }
-           
         }
-        
     }
 }
