0ad37ee7698bb0a55473eae51f0de230bdfb3c8b
Ayakben <47049290+Ayakben@users.noreply.github.com>
2020-01-31 23:25:15 -0500

    Fixed Drag and Drop

--- Game Jam Game/Assets/Scripts/DragUI.cs
+++ Game Jam Game/Assets/Scripts/DragUI.cs
@@ -5,6 +5,7 @@ using System.Collections;
 
 public class DragUI : MonoBehaviour
 {
+    Rigidbody2D shape;
     private bool clicked = false;
     void OnMouseOver()
     {
@@ -13,17 +14,23 @@ public class DragUI : MonoBehaviour
             clicked = true;
         }
     }
-
+    private void Start()
+    {
+        //Sets variable shape to the current objects rigidbody to change gravity
+        shape = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         if (clicked == true)
         {
             Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector2(cursorPos.x, cursorPos.y);
+            shape.gravityScale = 0;
         }
         if (Input.GetMouseButtonUp(0))
         {
             clicked = false;
+            shape.gravityScale = 1;
         }
     }
 }
