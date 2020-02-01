8ade8b9a5078160c2aa3da91a25d14af0ea897e8
Ayakben <47049290+Ayakben@users.noreply.github.com>
2020-02-01 01:56:28 -0500

    Added Score Text

---
+++ Game Jam Game/Assets/Scripts/SetScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetScore : MonoBehaviour
+{
+    public int score;
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
