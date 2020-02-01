28f6ae5e2283d82df62f02057437b756240439fa
Ayakben <47049290+Ayakben@users.noreply.github.com>
2020-02-01 17:33:02 -0500

    new script

--- Game Jam Game/Assets/Scripts/Generation.cs
+++ Game Jam Game/Assets/Scripts/Generation.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Generation : MonoBehaviour
 {
+    public int num = 0;
+    public GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(num < 2)
+        {
+            num++;
+            GameObject newGameObject = Instantiate(gameObject);
+        }
+
     }
 
     // Update is called once per frame
